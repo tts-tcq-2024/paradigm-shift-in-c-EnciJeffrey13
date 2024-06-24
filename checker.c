adb5d6c34ea613aa866dece2dadf1583a0e7993c
EnciJeffrey13 <ashleyjeffrey2011@gmail.com>
2024-06-24 20:47:11 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
-
-// Function to check if a value is within a specified range
-int isValueInRange(float value, float min, float max, const char* message) {
-  if(value < min || value > max) {
-    printf("%s out of range!\n", message);
+ 
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+  if(temperature < 0 || temperature > 45) {
+    printf("Temperature out of range!\n");
+    return 0;
+  } else if(soc < 20 || soc > 80) {
+    printf("State of Charge out of range!\n");
+    return 0;
+  } else if(chargeRate > 0.8) {
+    printf("Charge Rate out of range!\n");
     return 0;
   }
   return 1;
 }
-
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  return isValueInRange(temperature, 0, 45, "Temperature") &&
-         isValueInRange(soc, 20, 80, "State of Charge") &&
-         isValueInRange(chargeRate, 0, 0.8, "Charge Rate");
-}
-
+ 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
-  printf("All tests passed.\n");
-  return 0;
 }
