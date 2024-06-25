c2b548b2412e5968f1f156f2fb182034837ac8cc
EnciJeffrey13 <ashleyjeffrey2011@gmail.com>
2024-06-25 14:48:04 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <assert.h>
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if (temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  }
-  if (soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  }
-  if (chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
+int batteryIsOk(float temperature, float soc, float chargeRate) 
+{ 
+  int isTemperatureInRange = (temperature >= 0 && temperature <= 45); 
+  int isSocInRange = (soc >= 20 && soc <= 80); 
+  int isChargeRateInRange = (chargeRate <= 0.8); 
+  if (!isTemperatureInRange)
+  { 
+    printf("Temperature out of range!\n"); 
+  } 
+  if (!isSocInRange) 
+  {
+    printf("State of Charge out of range!\n"); 
+  } 
+  if (!isChargeRateInRange) 
+  { 
+    printf("Charge Rate out of range!\n"); 
+  } 
+  return isTemperatureInRange && isSocInRange && isChargeRateInRange; 
 }
 
 int main() {
