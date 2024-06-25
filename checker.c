f780e0a6b957a4ed7365ec7546381ab91173dd6c
EnciJeffrey13 <ashleyjeffrey2011@gmail.com>
2024-06-25 21:48:37 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -5,40 +5,40 @@ int main() {
     float temperature1 = 25, soc1 = 70, chargeRate1 = 0.7;
     float temperature2 = 50, soc2 = 85, chargeRate2 = 0;
 
-    // int isTemperatureInRange1 = (temperature1 >= 0 && temperature1 <= 45); 
-    // int isSocInRange1 = (soc1 >= 20 && soc1 <= 80); 
-    // int isChargeRateInRange1 = (chargeRate1 <= 0.8); 
+    int isTemperatureInRange1 = (temperature1 >= 0 && temperature1 <= 45); 
+    int isSocInRange1 = (soc1 >= 20 && soc1 <= 80); 
+    int isChargeRateInRange1 = (chargeRate1 <= 0.8); 
 
-    // int isAnyOutOfRange1 = !isTemperatureInRange1 || !isSocInRange1 || !isChargeRateInRange1;
-    // if (isAnyOutOfRange1) {
-    //     if (!isTemperatureInRange1) {
-    //         printf("Temperature out of range!\n");
-    //     }
-    //     if (!isSocInRange1) {
-    //         printf("State of Charge out of range!\n");
-    //     }
-    //     if (!isChargeRateInRange1) {
-    //         printf("Charge Rate out of range!\n");
-    //     }
+    int isAnyOutOfRange1 = !isTemperatureInRange1 || !isSocInRange1 || !isChargeRateInRange1;
+    if (isAnyOutOfRange1) {
+        if (!isTemperatureInRange1) {
+            printf("Temperature out of range!\n");
+        }
+        if (!isSocInRange1) {
+            printf("State of Charge out of range!\n");
+        }
+        if (!isChargeRateInRange1) {
+            printf("Charge Rate out of range!\n");
+        }
+    } 
+
+    assert(!isAnyOutOfRange1);
+
+    // int isTemperatureInRange2 = (temperature2 >= 0 && temperature2 <= 45); 
+    // int isSocInRange2 = (soc2 >= 20 && soc2 <= 80); 
+    // int isChargeRateInRange2 = (chargeRate2 <= 0.8); 
+
+    // if (!isTemperatureInRange2) { 
+    //     printf("Temperature out of range!\n"); 
+    // } 
+    // if (!isSocInRange2) { 
+    //     printf("State of Charge out of range!\n"); 
+    // } 
+    // if (!isChargeRateInRange2) { 
+    //     printf("Charge Rate out of range!\n"); 
     // } 
 
-    // assert(!isAnyOutOfRange1);
-
-    int isTemperatureInRange2 = (temperature2 >= 0 && temperature2 <= 45); 
-    int isSocInRange2 = (soc2 >= 20 && soc2 <= 80); 
-    int isChargeRateInRange2 = (chargeRate2 <= 0.8); 
-
-    if (!isTemperatureInRange2) { 
-        printf("Temperature out of range!\n"); 
-    } 
-    if (!isSocInRange2) { 
-        printf("State of Charge out of range!\n"); 
-    } 
-    if (!isChargeRateInRange2) { 
-        printf("Charge Rate out of range!\n"); 
-    } 
-
-    assert(!(isTemperatureInRange2 && isSocInRange2 && isChargeRateInRange2));
+    // assert(!(isTemperatureInRange2 && isSocInRange2 && isChargeRateInRange2));
 
     return 0;
 }
