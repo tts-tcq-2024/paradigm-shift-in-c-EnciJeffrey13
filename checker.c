898b36509383fd345811e7349f49730632583804
EnciJeffrey13 <ashleyjeffrey2011@gmail.com>
2024-06-27 11:44:00 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -3,25 +3,18 @@
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
     int isOk = 1;
-    const char *errorMessages[3] = { NULL, NULL, NULL };
 
     if (temperature < 0 || temperature > 45) {
-        errorMessages[0] = "Temperature out of range!\n";
+        printf("Temperature out of range!\n");
         isOk = 0;
     }
     if (soc < 20 || soc > 80) {
-        errorMessages[1] = "State of Charge out of range!\n";
+        printf("State of Charge out of range!\n");
         isOk = 0;
     }
     if (chargeRate > 0.8) {
-        errorMessages[2] = "Charge Rate out of range!\n";
+        printf("Charge Rate out of range!\n");
         isOk = 0;
-    }
-
-    for (int i = 0; i < 3; ++i) {
-        if (errorMessages[i] != NULL) {
-            printf("%s", errorMessages[i]);
-        }
     }
 
     return isOk;
