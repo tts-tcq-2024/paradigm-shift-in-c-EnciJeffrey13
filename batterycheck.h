81d7732a9074835601957e84e646dc0c2ece08c7
EnciJeffrey13 <ashleyjeffrey2011@gmail.com>
2024-07-25 12:22:07 +0530

    Update batterycheck.h

--- batterycheck.h
+++ batterycheck.h
@@ -5,12 +5,15 @@ typedef enum {
     TEMP_OK,
     SOC_OK,
     CHARGE_OK,
+    TEMP_WARNING,
+    SOC_WARNING,
+    CHARGE_WARNING,
     TEMP_OUT_OF_RANGE,
     SOC_OUT_OF_RANGE,
     CHARGE_OUT_OF_RANGE
 } BatteryStatus;
 
-const char* getErrorMessage(BatteryStatus status);
+const char* getErrorMessage(BatteryStatus status, const char* language);
 BatteryStatus checkBattery(float temperature, float soc, float chargeRate);
 int batteryIsOk(float temperature, float soc, float chargeRate);
 
