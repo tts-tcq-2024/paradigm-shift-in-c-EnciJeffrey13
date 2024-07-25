7c6680090a063ec84c84eccdc5ab302a35a743f3
EnciJeffrey13 <ashleyjeffrey2011@gmail.com>
2024-07-25 10:07:28 +0530

    Update batterytest.h

--- batterytest.h
+++ batterytest.h
@@ -1,1 +1,17 @@
+#ifndef BATTERY_CHECK_H
+#define BATTERY_CHECK_H
 
+typedef enum {
+    TEMP_OK,
+    SOC_OK,
+    CHARGE_OK,
+    TEMP_OUT_OF_RANGE,
+    SOC_OUT_OF_RANGE,
+    CHARGE_OUT_OF_RANGE
+} BatteryStatus;
+
+const char* getErrorMessage(BatteryStatus status);
+BatteryStatus checkBattery(float temperature, float soc, float chargeRate);
+int batteryIsOk(float temperature, float soc, float chargeRate);
+
+#endif
